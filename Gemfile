a2df0d8dfc73d920ab7864108826126ad825d246
bumble-bee-chuna <rebeccasheeler@gmail.com>
2024-10-10 14:21:23 -0700

    Add Popper gem and assets.

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem "stimulus-rails"
 gem "jbuilder"
 
 gem "bootstrap-sass"
+
+gem "sassc-rails"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,18 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+import "@popperjs/core";
+import "bootstrap";
+
+document.addEventListener("turbo:load", function () {
+  // This code is copied from Bootstrap's docs. See link below.
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
 
 //= require jquery
 //= require jquery_ujs
