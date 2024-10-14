2c52deca5ac4cb4a0782b6fe17e245d322b595a2
bumble-bee-chuna <rebeccasheeler@gmail.com>
2024-10-14 12:50:18 -0700

    Add Devise gem to enable authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :online_peer_and_recovery_resources
   resources :in_person_interest_resources
   resources :in_person_wellbeing_resources
